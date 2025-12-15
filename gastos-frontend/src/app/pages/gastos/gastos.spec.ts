4b8b7ca02180a33564f50e710da1c5c36db62e00
Jack <jackricardonarvaezsalazar@gmail.com>
2025-12-14 21:56:13 -0500

    Tests frontend

--- gastos-frontend/src/app/pages/gastos/gastos.spec.ts
+++ gastos-frontend/src/app/pages/gastos/gastos.spec.ts
@@ -2,22 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Gastos } from './gastos';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+
 describe('Gastos', () => {
   let component: Gastos;
   let fixture: ComponentFixture<Gastos>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Gastos]
-    })
-    .compileComponents();
+      imports: [Gastos, HttpClientTestingModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Gastos);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debe crear el componente', () => {
     expect(component).toBeTruthy();
   });
 });
